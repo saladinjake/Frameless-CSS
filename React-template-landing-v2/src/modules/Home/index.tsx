656cbbc9babb4d5009aa535eb916720dad6a6ba8
saladinjake <juwavictor@gmail.com>
2025-06-01 21:11:26 +0100

    chore: scroll animate

--- React-template-landing-v2/src/modules/Home/index.tsx
+++ React-template-landing-v2/src/modules/Home/index.tsx
@@ -1,4 +1,4 @@
-import { Fragment } from "react"
+import { Fragment, useLayoutEffect } from "react"
 import { WazoHeaderBranding } from "../../components/shared/Nav"
 import { WazoHeroBanner } from "../../components/shared/HeroBanner"
 import { WazoFeatures } from "../../components/shared/Features"
@@ -10,6 +10,22 @@ import { Faq } from "../../components/shared/FAQs"
 import { JoinMailingList } from "../../components/shared/JoinMailingList"
 import { Footer } from "../../components/shared/Footer"
 const HomeModule = () => {
+  useLayoutEffect(() => {
+    const observer = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.classList.add("animate");
+          }
+        });
+      },
+      { threshold: 0.1 }
+    );
+
+    document.querySelectorAll('[data-animate]').forEach(el => {
+      observer.observe(el);
+    });
+  }, [])
   return (
     <Fragment>
       <WazoHeaderBranding />
@@ -17,14 +33,14 @@ const HomeModule = () => {
         leadText="Receive, spend, and transfer with stablecoins."
         description="Make transactions to anyone at any time. Track your transactions "
         infoText="In real-time, keep control of your finances with ease and confidence " />
-        <WazoFeatures/>
-        <Services/>
-        <HowItWorks/>
-        <SecurityGrids/>
-        <StableCoinInformation/>
-        <Faq />
-        <JoinMailingList/>
-        <Footer/>
+      <WazoFeatures />
+      <Services />
+      <HowItWorks />
+      <SecurityGrids />
+      <StableCoinInformation />
+      <Faq />
+      <JoinMailingList />
+      <Footer />
     </Fragment>
 
   )
