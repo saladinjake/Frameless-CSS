80cd673f8cdf89dbda9587ca8c50f8c9fdb8074f
saladinjake <juwavictor@gmail.com>
2025-06-01 19:58:43 +0100

    chore: services component refactor

--- React-template-landing-v2/src/components/shared/Services.tsx
+++ React-template-landing-v2/src/components/shared/Services.tsx
@@ -1,47 +1,53 @@
-export const Services = () => {
+import { withReadyComponentObserver } from "../../utils/withReadyComponent";
 
+const ServiceCard = ({ title = "Send & Receive Funds", text = `Send money to friends, receive funds, and store easily using
+                            your Wazo account.`, icon = "./img/svg/sendmoney.svg" }: { title: string; icon: string; text: string }) => {
+
+    return (
+        <div className="card" data-animate="fade-in-up">
+            <div className="w-200 h-200 center">
+                <img src={icon} className="contain" />
+            </div>
+
+            <h3 className="text-md font-bold">{title}</h3>
+            <p className="text-sm mt-2 text-muted">{text}</p>
+        </div>
+    )
+}
+export const Services = withReadyComponentObserver(({ domReady }: { domReady: false }) => {
+    if (!domReady) return <>Loading...</>
     return (
 
         <section className="bg-dark text-white py-10">
             <div className="container">
-                <h2 className="text-center text-lg font-semibold mb-8">Everything you'll get on Wazo</h2>
+                <h2 className="text-center text-2xl font-semibold mb-8">Everything you'll get on Wazo</h2>
                 <div className="grid md:grid-cols-2 sm:grid-cols-1 gap-6">
-                    <div className="card" data-animate="fade-in-up">
-                        <div className="w-200 h-200 center">
-                            <img src="./public/svg/sendmoney.svg" className="contain" />
-                        </div>
-
-                        <h3 className="text-md font-bold">Send & Receive Funds</h3>
-                        <p className="text-sm mt-2 text-muted">Send money to friends, receive funds, and store easily using
-                            your Wazo account.</p>
-                    </div>
-                    <div className="card" data-animate="fade-in-up">
-                        <div className="w-200 h-200">
-                            <img src="./public/svg/wallet.svg" className="contain ml-8" />
-                        </div>
-                        <h3 className="text-md font-bold">Multi Pocket Wallet</h3>
-                        <p className="text-sm mt-2 text-muted">Manage and separate your finances in your wallet, allowing
-                            you to budget with ease.</p>
-                    </div>
-                    <div className="card" data-animate="fade-in-up">
-
-                        <div className="w-200 h-200">
-                            <img src="./public/svg/bill.svg" className="contain ml-8" />
-                        </div>
-                        <h3 className="text-md font-bold">Express Bill Payment</h3>
-                        <p className="text-sm mt-2 text-muted">Pay for your cable TV, electricity, internet, betting and
-                            more with just one tap in the app.</p>
-                    </div>
-                    <div className="card" data-animate="fade-in-up">
-                        <div className="w-200 h-200">
-                            <img src="./public/svg/save-and-earn.svg" className="contain ml-8" />
-                        </div>
-                        <h3 className="text-md font-bold">Save & Earn</h3>
-                        <p className="text-sm mt-2 text-muted">Grow your savings, avoid impulse debits, and earn interest
-                            while spending smarter.</p>
-                    </div>
+                    <ServiceCard
+                        title="Send & Receive Funds"
+                        text={`Send money to friends, receive funds, and store easily using
+                            your Wazo account.`}
+                        icon="./img/svg/sendmoney.svg"
+                    />
+                    <ServiceCard
+                        title="Multi Pocket Wallet"
+                        text={`Manage and separate your finances in your wallet, allowing
+                            you to budget with ease.`}
+                        icon="./img/svg/wallet.svg"
+                    />
+                    <ServiceCard
+                        title="Express Bill Payment"
+                        text={`Pay for your cable TV, electricity, internet, betting and
+                            more with just one tap in the app.`}
+                        icon="./img/svg/bill.svg"
+                    />
+                    <ServiceCard
+                        title="Save & Earn"
+                        text={`Grow your savings, avoid impulse debits, and earn interest
+                            while spending smarter.`}
+                        icon="./img/svg/save-and-earn.svg"
+                    />
                 </div>
             </div>
         </section>
     )
-}
+})
--- React-template-landing-v2/src/modules/Home/index.tsx
+++ React-template-landing-v2/src/modules/Home/index.tsx
@@ -2,7 +2,7 @@ import { Fragment } from "react"
 import { WazoHeaderBranding } from "../../components/shared/Nav"
 import { WazoHeroBanner } from "../../components/shared/HeroBanner"
 import { WazoFeatures } from "../../components/shared/Features"
-
+import { Services } from "../../components/shared/Services"
 const HomeModule = () => {
   return (
     <Fragment>
@@ -12,6 +12,7 @@ const HomeModule = () => {
         description="Make transactions to anyone at any time. Track your transactions "
         infoText="In real-time, keep control of your finances with ease and confidence " />
         <WazoFeatures/>
+        <Services/>
     </Fragment>
 
   )
